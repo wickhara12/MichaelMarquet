5185be9f133dada4a3168c1c7fb2dd13960c2819
wickhara12 <48776797+wickhara12@users.noreply.github.com>
2021-05-11 11:49:05 +1200

    Add files via upload

---
+++ javascripts/MySlide.js
@@ -0,0 +1,14 @@
+var myIndex = 0;
+SlidesIndexes();
+
+function SlidesIndexes() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(SlidesIndexes, 6000); // Change image every 2 seconds
+}
